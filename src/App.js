2db75d5f6135c7cb554063751cd994414c686560
Margie Panchal <panchal.hitendrabhai@tecnotree.com>
2024-09-12 22:15:22 +0530

    First commit of Control Deck

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Welcom to the Control Deck</h1>
+     <p>This platform allows authorized users to view, update, and delete employee details, ensuring secure access based on user permissions.</p>
     </div>
   );
 }
