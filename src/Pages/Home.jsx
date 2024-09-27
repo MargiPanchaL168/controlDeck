eb82076abf6a6c8d0c0a186f8246e65aff44aa19
Margie Panchal <panchal.hitendrabhai@tecnotree.com>
2024-09-27 13:23:46 +0530

    loader implimented

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,30 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Box from "@mui/material/Box";
 import {Typography, Grid2} from "@mui/material";
 import { ThemeContext } from "../App";
 import CardComponent from "../components/Card";
-import people from '../MockData/people.json'
+import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
 
 function Home() {
   const { theme } = useContext(ThemeContext);
-  const [peopleData, setPeopleData] = useState(people?.results)
-  console.log(peopleData);
+  const [peopleData, setPeopleData] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then(function (response) {
+        setPeopleData(response?.data?.results);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        setLoader(false);
+      });
+  }, []);
   return (
     <>
       <NavBar />
@@ -21,14 +36,17 @@ function Home() {
           height: "100%",
         }}
       >
-        <Grid2 container >
-        {peopleData?.map((p, index) => (
-          <Grid2 item key={index}>
-            <CardComponent data={p} />
-          </Grid2>
-          
-          ))}
-        </Grid2>
+        {loader ?  <CircularProgress /> : 
+        ( <Grid2 container >
+          {peopleData?.map((p, index) => (
+            <Grid2 item key={index}>
+              <CardComponent data={p} />
+            </Grid2>
+            
+            ))}
+          </Grid2>)
+        }
+       
       </Box>
     </>
   );
